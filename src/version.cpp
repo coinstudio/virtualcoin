7b524e0808e7a4986084177f5e904cda4fce45de
vcoin-z <u444665@yahoo.com>
2014-07-14 10:52:48 -0700

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("VCoin");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "0"
+#    define GIT_COMMIT_ID "54182cb"
 #    define GIT_COMMIT_DATE "Mon, 7 Jul 2014 08:01:20 -0700"
 #endif
 
